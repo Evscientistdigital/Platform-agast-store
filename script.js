6a4ab07db16eb3f5f8f323ca830a6d467c97168e
Evscientistdigital <evscientistdigital@gmail.com>
2026-03-16 04:56:45 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,78 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+updateCart();
+
+function addCart(name,price){
+
+cart.push({name,price});
+
+localStorage.setItem("cart",JSON.stringify(cart));
+
+updateCart();
+
+alert("Produk masuk keranjang");
+
+}
+
+function updateCart(){
+
+document.getElementById("cart-count").innerText = cart.length;
+
+}
+
+function openCart(){
+
+document.getElementById("cart-modal").style.display="block";
+
+let list = document.getElementById("cart-items");
+
+list.innerHTML="";
+
+let total=0;
+
+cart.forEach(item=>{
+let li=document.createElement("li");
+li.innerText=item.name+" - Rp "+item.price;
+list.appendChild(li);
+total+=item.price;
+});
+
+document.getElementById("total").innerText=total;
+
+}
+
+function closeCart(){
+document.getElementById("cart-modal").style.display="none";
+}
+
+function checkout(){
+
+alert("Checkout berhasil!");
+
+cart=[];
+
+localStorage.removeItem("cart");
+
+updateCart();
+
+closeCart();
+
+}
+
+function showLogin(){
+document.getElementById("login-modal").style.display="block";
+}
+
+function closeLogin(){
+document.getElementById("login-modal").style.display="none";
+}
+
+function login(){
+
+let user=document.getElementById("username").value;
+
+alert("Selamat datang "+user+" di Agast Store");
+
+closeLogin();
+
+}
